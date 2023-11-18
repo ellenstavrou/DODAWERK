1bb40b117f343e077d38179a5e293daaf2b762bc
ellenstavrou <ellenstavrou@gmail.com>
2023-11-18 21:27:48 +0000

    Initial Commit

---
+++ DODAWERK/DODAWERKApp.swift
@@ -0,0 +1,17 @@
+//
+//  DODAWERKApp.swift
+//  DODAWERK
+//
+//  Created by Ellen Stavrou on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DODAWERKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
